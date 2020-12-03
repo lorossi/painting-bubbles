c2f531c253074afba6f66e9e28664e758e9a1285
lorossi <lo.rossi@live.com>
2020-12-03 18:50:40 +0100

    now works even with unevely sized paintings

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* jshint esversion: 8 */
 
-class Rect {
+class Circle {
   constructor(x, y, width, height, color, split_direction) {
     this.x = x;
     this.y = y;
@@ -12,17 +12,14 @@ class Rect {
     this._color = color;
     this._split_direction = split_direction || "horizontal";
 
+    this.min_radius = 5;
+    this._min_size = this.r > this.min_radius;
+
     this._just_split = true;
     this._age_count = 0;
-    // how many times you have to move the mouse on the screen before the rect gets popped
-    if (this.width > 200) {
-      this._split_age = 20;
-    } else if (this.width > 50) {
-        this._split_age = 10;
-    } else if (this.width > 5) {
-        this._split_age = 5;
-      }
-  }
+    // how many times you have to move the mouse on the screen before the circle gets popped
+    this._split_age = this.r * .75;
+}
 
   get pos() {
     return {
@@ -67,24 +64,33 @@ class Rect {
     }
   }
 
+  get min_size() {
+    return this._min_size;
+  }
+
 }
 
 
 class Sketch {
-  constructor(canvas, context) {
+  constructor(canvas, context, source_width, source_height) {
     this.canvas = canvas;
     this.ctx = context;
     this.width = canvas.width;
     this.height = canvas.height;
+    this._source_width = source_width;
+    this._source_height = source_height;
 
     this.mouse_x = 0;
     this.mouse_y = 0;
-    this.min_size = 10;
 
     // pixels container
     this._pixels = [];
     // rects container
-    this._rects = [];
+    this._circles = [];
+
+    // displacement
+    this.dx = Math.floor((this.width - this._source_width) / 2);
+    this.dy = Math.floor((this.height - this._source_height) / 2);
 
     // save canvas in memory
     this.savedData = new Image();
@@ -112,7 +118,7 @@ class Sketch {
     this.height = this.canvas.height;
   }
 
-  clicked(x, y) {
+  splitCircle(x, y) {
     if (x === this.mouse_x && y === this.mouse_y) {
       return;
     }
@@ -122,12 +128,12 @@ class Sketch {
     let found, pos, i, just_split;
 
     found = false;
-    for (i = 0; i < this._rects.length; i++) {
-      pos = this._rects[i].pos;
-      if (x >= pos.x && y >= pos.y && x <= pos.x + pos.width && y <= pos.y + pos.height && pos.width > this.min_size) {
-        just_split = this._rects[i].just_split;
+    for (i = 0; i < this._circles.length; i++) {
+      pos = this._circles[i].pos;
+      if (x >= pos.x && y >= pos.y && x <= pos.x + pos.width && y <= pos.y + pos.height && !pos.min_size) {
+        just_split = this._circles[i].just_split;
         if (just_split) {
-          this._rects[i].just_split = false;
+          this._circles[i].just_split = false;
           break;
         }
         found = true;
@@ -138,33 +144,33 @@ class Sketch {
     if (found) {
       // at least one has been found
       let split_direction, next_split_direction;
-      split_direction = this._rects[i].split_direction;
+      split_direction = this._circles[i].split_direction;
       if (split_direction === "horizontal") {
         next_split_direction = "vertical";
       } else if (split_direction === "vertical"){
         next_split_direction = "horizontal";
       }
 
-      // remove old rect
-      this._rects.splice(i, 1);
+      // remove old circle
+      this._circles.splice(i, 1);
 
       // new position and size
       let nx, ny, nwidth, nheight;
       for (i = 0; i < 2; i++) {
         if (split_direction === "horizontal") {
           nx = pos.x;
-          ny = Math.floor(pos.y + pos.height / 2 * i);
+          ny = pos.y + pos.height / 2 * i;
           nwidth = pos.width;
-          nheight = Math.floor(pos.height / 2);
+          nheight = pos.height / 2;
         }  else if (split_direction === "vertical"){
-          nx =  Math.floor(pos.x + pos.width / 2 * i);
+          nx = pos.x + pos.width / 2 * i;
           ny = pos.y;
-          nwidth = Math.floor(pos.width / 2);
+          nwidth = pos.width / 2;
           nheight = pos.height;
         }
-        let average_color = this.averageColor(nx, ny, nwidth, nheight, true);
-        let new_rect = new Rect(nx, ny, nwidth, nheight, average_color, next_split_direction);
-        this._rects.push(new_rect);
+        let average_color = this.averageColor(nx - this.dx, ny - this.dy, nwidth, nheight, true);
+        let new_rect = new Circle(nx, ny, nwidth, nheight, average_color, next_split_direction);
+        this._circles.push(new_rect);
       }
     }
   }
@@ -174,12 +180,13 @@ class Sketch {
   }
 
   draw() {
-    // if rect is empty, it's time to generate a new rect
-    if (this._rects.length === 0) {
-      let average_color = this.averageColor(0, 0, this.width, this.height);
-      let new_rect = new Rect(0, 0, this.width, this.height, average_color);
-      this._rects.push(new_rect);
+    // if circle is empty, it's time to generate a new circle
+    if (this._circles.length === 0) {
+      let average_color = this.averageSource();
+      let new_rect = new Circle(this.dx, this.dy, this.width - this.dx * 2, this.height - this.dy * 2, average_color);
+      this._circles.push(new_rect);
       this.background = new_rect.color;
+      setCssProperty("--background-color", this.background);
     }
 
     this.ctx.save();
@@ -191,7 +198,7 @@ class Sketch {
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
     // draw rects
-    this._rects.forEach((r, i) => {
+    this._circles.forEach((r, i) => {
       let rect_pos;
       rect_pos = r.pos;
       this.ctx.fillStyle = r.color;
@@ -206,9 +213,9 @@ class Sketch {
     window.requestAnimationFrame(this.draw.bind(this));
   }
 
-  get_pixel(x, y) {
+  getPixel(x, y) {
     let index, pixel;
-    index = y * this.width * 4 + x * 4;
+    index = y * this._source_width * 4 + x * 4;
     pixel = [];
     for (let i = 0; i < 4; i++) {
       pixel.push(this._pixels[index+i]);
@@ -216,28 +223,36 @@ class Sketch {
     return pixel;
   }
 
-  averageColor(x0, y0, width, height, show_all) {
+  averageColor(x0, y0, width, height) {
+    // make all parameters integer
+    x0 = Math.round(x0);
+    y0 = Math.round(y0);
+    width = Math.round(width);
+    height = Math.round(height);
     // current rectangle size
     let rect_size;
     rect_size = width * height;
-    // sum of all pixels in rect, current pixel
+    // sum of all pixels in circle, current pixel
     let rect_pixels, current_pixel;
     // only r, g, b (discard alpha)
     rect_pixels = [0, 0, 0];
 
     for (let x = x0; x < x0 + width; x++) {
       for (let y = y0; y < y0 + height; y++) {
-        current_pixel = this.get_pixel(x, y);
+        current_pixel = this.getPixel(x, y);
         for (let j = 0; j < rect_pixels.length; j++) {
           rect_pixels[j] += current_pixel[j];
         }
       }
     }
-
     // average color
     let average_color;
     average_color = rect_pixels.map(x => x / rect_size);
     return average_color;
+  }
+
+  averageSource() {
+    return this.averageColor(0, 0, this._source_width, this._source_height);
   }
 
   get pixels() {
@@ -270,14 +285,14 @@ let load_pixels = (img_src, canvas_size) => {
 
       // create hidden canvas to load pixels
       $(".container").append('<canvas class="hidden" id="hidden"></canvas>');
-      $("canvas#hidden").prop({"width": canvas_size, "height": canvas_size});
+      $("canvas#hidden").prop({"width": image_width * scl, "height": image_height * scl});
       // load (hidden) canvas
       canvas = document.getElementById("hidden");
 
       // displacement values to center the image
       let dx, dy;
-      dx = (canvas.width - image_width * scl) / 2;
-      dy = (canvas.height - image_height * scl) / 2;
+      dx = Math.floor((canvas_size - image_width * scl) / 2);
+      dy = Math.floor((canvas_size - image_height * scl) / 2);
 
       if (canvas.getContext) {
         ctx = canvas.getContext("2d", {alpha: false});
@@ -289,7 +304,7 @@ let load_pixels = (img_src, canvas_size) => {
         ctx.fillStyle = getCssProperty("--background-color");
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         // draw image
-        ctx.drawImage(img, 0, 0, image_width, image_height, dx, dy, image_width * scl, image_height * scl);
+        ctx.drawImage(img, 0, 0, image_width, image_height, 0, 0, image_width * scl, image_height * scl);
         // load pixels
         image_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
       }
@@ -311,21 +326,20 @@ let main = async () => {
   canvas_size = 900;
   img_path = "/assets/paintings/gioconda.jpg";
 
+  let pixels;
+  pixels = await load_pixels(img_path, canvas_size);
+
   // resize canvas
   $(`canvas#${sketch_id}`).prop({
     "width": canvas_size,
-    "height": canvas_size
+    "height": canvas_size,
   });
-
-  let pixels;
-  pixels = await load_pixels(img_path, canvas_size);
-  console.log(pixels)
 
   let canvas, ctx;
   canvas = document.getElementById(sketch_id);
   if (canvas.getContext) {
     ctx = canvas.getContext("2d", {alpha: false});
-    sketch = new Sketch(canvas, ctx);
+    sketch = new Sketch(canvas, ctx, pixels.width, pixels.height);
     sketch.pixels = pixels.data;
     sketch.run();
   }
@@ -346,7 +360,7 @@ $(window).resize(() => {
 $(document).mousemove((e) => {
   if (e.target.id === sketch_id) {
     if (sketch) {
-      sketch.clicked(e.offsetX, e.offsetY);
+      sketch.splitCircle(e.offsetX, e.offsetY);
     }
   }
 });
