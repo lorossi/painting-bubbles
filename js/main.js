6a6546a0c2f55d2f61625b4b3fe459a882735360
lorossi <lo.rossi@live.com>
2020-12-05 17:01:18 +0100

    recording fine tunings, approaching final version

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ let sketch, capturer;
 // sketch container id
 let sketch_id = "sketch";
 // make this true to start recording
-let recording = false;
+let recording = true;
 // make this true to go automatically
-let auto = false;
+let auto = true;
 // current path
 let current_path;
 // are we on mobile?
@@ -122,15 +122,15 @@ $(document).ready(() => {
       next_image();
     }
   });
-});
 
-// resize window event
-$(window).resize(() => {
-  get_canvas_size();
-  resize_canvas();
-  if (sketch) {
-    sketch.resized();
-  }
+  // resize window event
+  $(window).resize(() => {
+    get_canvas_size();
+    resize_canvas();
+    if (sketch) {
+      sketch.resized();
+    }
+  });
 });
 
 // loads next image in path
@@ -139,10 +139,15 @@ $(window).resize(() => {
 // direction = 0 -> reset
 // direction = "random" -> random
 let next_image = async (direction) => {
-  if (recording && current_path === names.length - 1) {
+  if (recording && current_path === names.length) {
     console.log("%cAll paintings have been recorded", "color:red;font-size:1.5rem;");
     return;
+  } else if (recording) {
+    await capturer.stop();
+    await capturer.save();
+    console.log(`%cRecorded painting ${current_path + 1}/${names.length}`, "color:green;font-size:1rem;");
   }
+
   if (direction === undefined) {
     direction = 1;
   } else if (direction === "random") {
@@ -157,10 +162,22 @@ let next_image = async (direction) => {
   // load pixels
   let pixels;
   pixels = await load_pixels(img_path, canvas_size);
+
   // put pixels inside sketch
   sketch.pixels = pixels.data;
   sketch.source_width = pixels.width;
   sketch.source_height = pixels.height;
+
+  // fire up recorder again
+  if (recording) {
+    capturer = new CCapture({
+                             format: "png",
+                             name: `${names[current_path].replace("-", " ").replace(".jpg", "")}-${current_path+1}`,
+                             autoSaveTime: 30,
+                             frameRate: 60
+                            });
+  }
+
   // reset sketch
   sketch.reset();
   // reload sketch
@@ -236,7 +253,7 @@ class Circle {
     if (mobile) {
       this.min_radius = 2;
     } else {
-      this.min_radius = 4;
+      this.min_radius = 2;
     }
 
     // check if the circle is already of the minimum size
@@ -249,7 +266,6 @@ class Circle {
     } else {
       this._split_age = 3;
     }
-
 
     if (!split_direction) {
       // it's the first circle, we have to set the direction of its split
@@ -330,7 +346,7 @@ class Sketch {
     this.fps_interval = 1000 / this.fps;
 
     // how many circles should be selected for splitting
-    this.auto_to_keep = 35;
+    this.auto_to_keep = 50;
     // amout of split circles
     this.split_circles = 0;
     // pixels container
@@ -365,6 +381,8 @@ class Sketch {
   reset() {
     // reset circles array
     this._circles = [];
+    // reset ended status
+    this._ended = false;
   }
 
   save() {
@@ -396,12 +414,16 @@ class Sketch {
   }
 
   mouseMoved(e) {
+    if (recording) return;
+
     let mouse_coords;
     mouse_coords = get_mouse_pos(this.canvas, e);
     this.searchCircle(mouse_coords.x, mouse_coords.y);
   }
 
   touchMoved(e) {
+    if (recording) return;
+
     let touch_coords;
     touch_coords = get_touch_pos(this.canvas, e);
     this.searchCircle(touch_coords.x, touch_coords.y);
@@ -469,7 +491,7 @@ class Sketch {
     // load background
     this.background = get_css_property("--background-color");
     if (capturer) {
-      // start capturing
+      // start capturer
       capturer.start();
     }
     // save time to limit fps
@@ -488,16 +510,12 @@ class Sketch {
       }
     }
 
-    if ((!this.ended && this._circles.length == 1) || auto) {
+    if (!this._ended && (this._circles.length == 1 || recording)) {
       // load the next frame
       window.requestAnimationFrame(this.draw.bind(this));
-    } else if (this.ended &&  recording) {
+    } else if (this._ended && recording) {
         // if in auto or recording, load another image
         // if recording, stop and save
-      if (capturer) {
-        capturer.stop();
-        capturer.save();
-      }
       next_image();
       return;
       // otherwise, the user has to do so manually
@@ -540,7 +558,7 @@ class Sketch {
         this.ctx.shadowOffsetX = 5;
         this.ctx.shadowOffsetY = 5;
         this.ctx.shadowBlur = 15;
-      }else if (c.pos.r > 25) {
+      } else if (c.pos.r > 25) {
         this.ctx.shadowOffsetX = 3;
         this.ctx.shadowOffsetY = 3;
         this.ctx.shadowBlur = 5;
@@ -556,10 +574,13 @@ class Sketch {
         this.ctx.shadowBlur = 0;
       }
 
+      this.ctx.save();
       this.ctx.fillStyle = c.color;
       this.ctx.beginPath();
       this.ctx.arc(circle_pos.cx, circle_pos.cy, circle_pos.r, 0, 2 * Math.PI);
       this.ctx.fill();
+      this.ctx.restore();
+
       if (all_min_size && !c.min_size) {
         all_min_size = false;
       }
@@ -568,7 +589,6 @@ class Sketch {
 
     // save frame if recording
     if (recording) {
-      // async function
       this.captureFrame();
     }
 
@@ -577,19 +597,19 @@ class Sketch {
       // circles big enough to be split
       let available_circles;
       available_circles = this._circles.filter(c => !c.min_size);
+      available_circles = available_circles.sort((a, b) => (b.r - a.r));
+      available_circles = available_circles.slice(0, this.auto_to_keep);
 
-      // how many circles are split each time
-      let iterations = Math.floor(this.split_circles / 10 - 180);
-      iterations = iterations <= 0 ? 1 : iterations;
-      // sort by size
-      available_circles.sort((c1, c2) => parseInt(c2.r) - parseInt(c1.r));
-      // keep only the first n
-      let to_keep;
-      // don't need to keep more than what we have
-      to_keep = Math.min(available_circles.length, this.auto_to_keep);
-      available_circles = available_circles.slice(0, to_keep);
 
-      for (let i = 0; i < Math.min(iterations, available_circles.length); i++) {
+      let iterations;
+      if (this.split_circles < 900) {
+        iterations = Math.floor(this.split_circles / 300);
+        iterations = iterations <= 0 ? 1 : iterations;
+      } else {
+        iterations =  Math.floor(this._circles.length / 25);
+      }
+
+      for (let i = 0; i < iterations; i++) {
          let random_index, circles_index;
           random_index = random(0, available_circles.length - 1, true);
           circles_index = this._circles.indexOf(available_circles[random_index]);
@@ -603,7 +623,7 @@ class Sketch {
       }
 
     // all the circles are small!
-    if (all_min_size && !this.ended) {
+    if (all_min_size && !this._ended) {
       // the sketch has ended
       this._ended = true;
     }
