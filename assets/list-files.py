13b83bba454cfce17217bf6d2bb4b3165618a29c
lorossi <lo.rossi@live.com>
2020-12-09 15:28:49 +0100

    added script to generate the list of paintings in the form of a js constant

---
+++ assets/list-files.py
@@ -0,0 +1,12 @@
+import json
+import os
+import random
+
+# this scripts generate the list of paintings in the form of a js constant
+# run this and copy the output in main.js
+# this is needed because js can't access root tree and I won't write each
+# filename inside the script
+
+images = os.listdir("paintings/")
+js_variable = f"const names = {json.dumps(images)};"
+print(js_variable)
